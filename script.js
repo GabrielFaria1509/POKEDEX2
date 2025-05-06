2631e8bcda42259c1fa5fcd37a46dfe662c7fc2f
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-05-05 23:06:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,32 @@ const fetchPokemon = async (pokemon) => {
     
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
 
-    const data = await APIResponse.json();
-    
+    if(APIResponse.status === 200){
+        const data = await APIResponse.json();
+        return data;
+    }    
     return data;
 }
 
 const renderPokemon = async  (pokemon) => {
 
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+    
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if(data){
+       pokemonName.innerHTML = data.name;
+       pokemonNumber.innerHTML = data.id;
+       pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
-    input.value = '';
+       input.value = '';
+
+    }else{
+        pokemonName.innerHTML = 'Not Found :c';
+        pokemonNumber.innerHTML = '';
+        
+    }
 }
 
 form.addEventListener('submit', (event) => {
@@ -29,8 +41,7 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     renderPokemon(input.value);
-    
-
-
 });
+
+renderPokemon('1'); // Load the first Pokemon by default
 
