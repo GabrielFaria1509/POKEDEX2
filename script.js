c2b0efb336625bc0cac4d1332ae4878d70851a93
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-05-07 21:22:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
+
 const pokemonImage = document.querySelector('.pokemon_image');
 const form = document.querySelector('.form')
 const input = document.querySelector('.input_search');
 
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
 const fetchPokemon = async (pokemon) => {
     
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
     if(APIResponse.status === 200){
         const data = await APIResponse.json();
         return data;
     }    
-    return data;
+    
 }
 
 const renderPokemon = async  (pokemon) => {
@@ -23,13 +29,15 @@ const renderPokemon = async  (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     if(data){
+       pokemonImage.style.display = 'block';  // Show the image when data is found
        pokemonName.innerHTML = data.name;
        pokemonNumber.innerHTML = data.id;
        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-
        input.value = '';
+       searchPokemon = data.id; // Update the searchPokemon variable with the current Pokemon ID
 
     }else{
+        pokemonImage.style.display = 'none';     // Hide the image when data is not found
         pokemonName.innerHTML = 'Not Found :c';
         pokemonNumber.innerHTML = '';
         
@@ -40,8 +48,20 @@ form.addEventListener('submit', (event) => {
     
     event.preventDefault();
 
-    renderPokemon(input.value);
+    renderPokemon(input.value.toLowerCase());
 });
 
-renderPokemon('1'); // Load the first Pokemon by default
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon); // Load the first Pokemon by default
 
